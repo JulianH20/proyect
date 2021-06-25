e7ae65d2831428d7d779bb63589af19265b5b422
Julian Hernandez <julian.h.castellanos06@gmail.com>
2021-06-24 19:27:24 -0500

    Chore: Update indez

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola Mundo\n');
+    res.end('Este es mi proyecto\n');
   });
 
   server.listen(port, hostname, () => {
